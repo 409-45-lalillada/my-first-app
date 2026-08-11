329eb7c31016a50fc1da866c9aeed8d2f0a715ba
409-45-lalillada <56967@yupparaj.ac.th>
2026-08-11 14:26:09 +0700

    Update bmiApp.py

--- bmiApp.py
+++ bmiApp.py
@@ -7,15 +7,14 @@ height_cm = st.number_input("กรอกน้ำส่วนสูงของ
 if st.button("คำนวณค่า BMI🎯"):
   height_m = height_cm / 100
   bmi = weight / (height_m ** 2)
-
-st.write("---")
-st.header(f"ค่า BMI ของคุณคือ: **{bmi:.2f}**")
+  st.write("---")
+  st.header(f"ค่า BMI ของคุณคือ: **{bmi:.2f}**")
 
 if bmi < 18.5:
   st.warning("⚠️คุณมีน้ำหนักน้อยกว่าเกณฑ์ (ผอม)")
-elif 18.5 <= BMI < 23.0:
+elif 18.5 <= bmi < 23.0:
   st.success("🎉คุณมีน้ำหนักอยู่ในเกณฑ์ปกติ (สุขภาพดี)")
-elif 23.0 <= BMI < 25.0:
+elif 23.0 <= bmi < 25.0:
   st.info("💡คุณเริ่มมีน้ำหนักเกินเกณฑ์ (ท้วม)")
 else:
   st.error("🚨คุณอยู่ในเกณฑ์อ้วน ควรระวังเรื่องสุขภาพและการออกกำลังกาย")
